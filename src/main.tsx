e5e7134ead48b325af839203f00ac7d6a52c4a5e
BDeuDev <bdeusich.dev@gmail.com>
2024-05-07 21:52:39 -0300

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
+    <BrowserRouter basename="/">
     <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
   </Provider>
 )

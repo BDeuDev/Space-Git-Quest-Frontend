693ea9eceaa0c54bc5df486cbd108cd80a6f71c9
BDeuDev <bdeusich.dev@gmail.com>
2024-05-09 19:16:21 -0300

    changes

--- src/components/profile/game/npcChat.tsx
+++ src/components/profile/game/npcChat.tsx
@@ -13,26 +13,26 @@ const NpcChat = () => {
     //@ts-ignore
     const status = useSelector((state)=>state.status)
     console.log(status.status)
-    const [text,setText] = useState(`Viajando a ${init}...`)
+    const [text,setText] = useState(`Traveling to ${init}...`)
     useEffect(() => {
         
         if(called){
            const timer = setTimeout(() => {
-            setText("Nos encontramos en -->");
+            setText("We are currently in -->");
         }, 3000); 
 
         return () => clearTimeout(timer); 
         }else{
-            setText(`Viajando a ${init}...`)
+            setText(`Traveling to ${init}...`)
         }
         
     }, [called]);
     return (
         <div className="fixed bottom-[1%] right-[40%] w-auto h-auto left-[35%] shadow-2xl z-50 flex flex-col items-center justify-center">
             <div className="flex flex-row items-center justify-center absolute">
-                {status.called && <><h1 className="z-50  font-semibold text-green-600 inner_text_shadow mr-2">Ubicación actual {'-->'}{status.status}</h1>
+                {status.called && <><h1 className="z-50  font-semibold text-green-600 inner_text_shadow mr-2">Current location {'-->'}{status.status}</h1>
                     <Planet name={cd} /></>}
-                {active && <><h1 className="z-50  font-semibold text-green-600 inner_text_shadow mr-2">Viajar a {cd}?</h1>
+                {active && <><h1 className="z-50  font-semibold text-green-600 inner_text_shadow mr-2">Travel to {cd}?</h1>
                     <Planet name={cd} /></>}
                 {called && <><h1 className="z-50  font-semibold text-green-600 inner_text_shadow mr-2">{text}</h1>
                     <Planet name={cd} /></>}

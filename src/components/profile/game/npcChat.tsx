fc0ec13f72f39bf754aecb607325ff0761c665bd
BDeuDev <bdeusich.dev@gmail.com>
2024-05-10 12:02:25 -0300

    changes

--- src/components/profile/game/npcChat.tsx
+++ src/components/profile/game/npcChat.tsx
@@ -34,7 +34,7 @@ const NpcChat = () => {
                     <Planet name={cd} /></>}
                 {active && cd && <><h1 className="z-50  font-semibold text-green-600 inner_text_shadow mr-2">Travel to {cd}?</h1>
                     <Planet name={cd} /></>}
-                {active && !cd && <h1 className="z-50  font-semibold text-green-600 inner_text_shadow mr-2">Invalid planet name, if u forgot the planets's names do ls on the terminal</h1>}
+                {active && !cd && <h1 className="z-50  font-semibold text-green-600 inner_text_shadow mr-2">Planet name can't me empty</h1>}
                 {called && <><h1 className="z-50  font-semibold text-green-600 inner_text_shadow mr-2">{text}</h1>
                     <Planet name={cd} /></>}
                 {call && <div className="flex flex-col items-center justify-center w-full h-full">
